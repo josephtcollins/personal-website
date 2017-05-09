606e6f2e81bd0062cf8cfe71bc75681e80b02e24
Joe Collins <joe.collins121@gmail.com>
2017-05-09 11:10:16 -0600

    Changes and additions to content and styling

--- javascripts/animations/section-2.js
+++ javascripts/animations/section-2.js
@@ -2,14 +2,14 @@ var $ = window.jQuery = window.$ = require('jquery');
 var velocity = require('velocity-animate');
 
 module.exports = function () {
-    $('.section-2 .text')
-        .velocity({
-            left: '40%',
-            opacity: 0
-        }, 0)
-        .delay(100)
-        .velocity({
-            left: '50%',
-            opacity: 1
-        }, 600, 'easeOutCubic');
+    // $('.section-2 .text')
+    //     .velocity({
+    //         left: '40%',
+    //         opacity: 0
+    //     }, 0)
+    //     .delay(100)
+    //     .velocity({
+    //         left: '50%',
+    //         opacity: 1
+    //     }, 600, 'easeOutCubic');
 };
